c61ebd2a4a11134eadd2d1e7285a61c7766d169c
alexfinger21 <61606770+alexfinger21@users.noreply.github.com>
2024-06-02 12:30:37 -0400

    Update 1981A.cpp

--- 1981A.cpp
+++ 1981A.cpp
@@ -3,23 +3,6 @@
 
 using namespace std;
 
-int maxPrimeFactors(int n) {
-    int count = 0;
-
-    for (int i = 2; i<=int(sqrt(n)); ++i) {
-        while (n % i == 0) {
-            ++count;
-            n /= i;
-        }
-    }
-
-    if (n != 1) {
-        ++count;
-    }
-
-    return count;
-}
-
 int main() {
     int cases;
     cin >> cases;
@@ -28,21 +11,7 @@ int main() {
         int min, max;
         cin >> min >> max;
 
-        int res = 0;
-
-        cout << "hi" << endl;
-        cout << (double)((max/min - 1)) / (max/min) << endl;
-        cout << (int)(min+(max-min)*(double)((max/min - 1) / (max/min))) << endl;
-        
-        for (int i = max; i>=(int)(min+(max-min)*(double)((max/min - 1) / (max/min))); --i) {
-            int maxP = maxPrimeFactors(i);
-            if (maxP == 20) {
-                cout << i << " : " << maxP << endl; 
-            }
-            if (maxP > res) {
-                res = maxP;
-            }
-        }
+        int res = log2(max);
 
         cout << res << '\n';
     }
