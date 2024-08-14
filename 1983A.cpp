62d6b67161b07487cbf8d7afea003b70f8d65ba1
alexfinger21 <61606770+alexfinger21@users.noreply.github.com>
2024-08-14 18:30:19 -0400

    Add files via upload

---
+++ 1983A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using ll=long long;
+using namespace std;
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        for (int i = 0; i<n; ++i) {
+            cout << i+1 << ' ';
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
