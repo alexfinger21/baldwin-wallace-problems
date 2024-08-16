7e7f76f7a6ca58087b801089399f2673dcb78f77
alexfinger21 <61606770+alexfinger21@users.noreply.github.com>
2024-08-15 23:32:18 -0400

    Add files via upload

--- 2000F.cpp
+++ 2000F.cpp
@@ -3,70 +3,47 @@
 using ll=long long;
 using namespace std;
 
+int inf = (int)(1e9);
+
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
     cin >> t;
 
+
     while (t--) {
         int n, k;
         cin >> n >> k;
+        vector<int> dp(k+1, inf);
 
-        vector<pair<int, int>> rows;
-        int rcsum = 0;
+        dp[0] = 0;
 
-        for (int i = 0; i<n; ++i) {
-            int w, h;
-            cin >> w >> h;
+        while (n--) {
+            vector<int> cost_a(k+1, inf);
+            cost_a[0] = 0;
+            int a, b, cost = 0, cnt = 0;
+            cin >> a >> b;
+            
+            while (k > cnt && (a > 0 || b > 0)) {
+                if (a < b) {
+                    swap(a, b);
+                }
 
-            rcsum+=w+h;
-            rows.push_back(make_pair(w, h));
-        }
+                cost += b;
+                ++cnt;
+                cost_a[cnt] = cost;
+                --a;
+            }
 
-        if (rcsum < k) {
-            cout << "-1\n";
-            continue;
-        }
-
-        sort(rows.begin(), rows.end(), [](auto &left, auto &right) {
-            return min(left.second, left.first) < min(right.first, right.second);
-        });
-
-        int ops = 0;
-        int colored = 0;
-        for (int i = 0; i<rows.size(); ++i) {
-            if (colored < k) {
-                auto pr = rows[i];
-                cout << pr.first << ' ' << pr.second << endl;
-                int small = min(pr.first, pr.second);
-                int big = max(pr.first, pr.second);
-
-                int diff = big-small;
-
-                ops += min(k-colored, diff+1)*small;
-                colored += min(k-colored, diff+1);
-                cout << colored << endl;
-
-                if (colored < k) {
-                    for (int j = small-1; j>0; --j) {
-                        ops += j*min(k-colored, 2);
-                        colored += min(k-colored, 2);
-                        if (j == 1) {
-                            ++colored;
-                        }
-                        cout << ops << endl;
-                        if (colored > k) {
-                            break;
-                        }
-                    }
-                } else {
-                    break;
+            for (int i = k - 1; i>=0; --i) {
+                for (int j = 1; j<=k-i; ++j) {
+                    dp[i+j] = min(dp[i+j], dp[i] + cost_a[j]);
                 }
             }
         }
 
-        cout << ops << endl;
+        cout << (dp[k] == inf ? -1 : dp[k]) << '\n';
     }
 
     return 0;
