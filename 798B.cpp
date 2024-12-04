c871d143f889444ce9f213ae4c3457e37aef76d6
alexfinger21 <61606770+alexfinger21@users.noreply.github.com>
2024-12-03 20:58:12 -0500

    Add files via upload
    basically og dp arr contains all vars of first string, and then all others try to optimize for it, so first dimension is first i strings and second dimension is number of shifts of the original, and the meaning is the min number of operations on the first i strings to equal first string at j operations.

--- 798B.cpp
+++ 798B.cpp
@@ -6,60 +6,51 @@ using namespace std;
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
     int n;
     cin >> n;
-    
+
     vector<string> a(n);
-    set<char> uniq;
 
     for (int i = 0; i<n; ++i) {
         cin >> a[i];
     }
 
-    ll res = 1e9;
+    int l = a[0].size();
 
-    bool fnd = false;
-    for (int i = 0; i<a[0].size(); ++i) {
-        ll rs = 0;
-        if (fnd) {
-            break;
-        }
-        for (auto s : a) {
-            if (s == a[0]) {
-                rs += i;
-            } else {
-                int f = s.find(a[0][i]);
-                int lr = -1;
-                while (f != string::npos) {
-                    int j;
-                    for (j = 1; j<a[0].size(); ++j) {
-                        if (a[0][(i+j)%a[0].size()] != s[(f+j)%a[0].size()]) {
-                            break;
-                        }
-                    }
+    vector<vector<int>> dp(n, vector<int>(l, 1e9));
 
-                    if (j == a[0].size()) {
-                        lr = lr != -1 ? min(lr, f) : f;
-                        break;
-                    }
-                    f = s.find(a[0][i], f+1);
-                }
-                if (lr == -1) {
-                    fnd = true;
+    for (int i = 0; i<l; ++i) {
+        dp[0][i] = i;
+    }
+
+    for (int i = 1; i<n; ++i) {
+        for (int k = 0; k<l; ++k) {
+            string s1 = a[0].substr(k, l-k) + a[0].substr(0, k);
+            for (int j = 0; j<l; ++j) {
+                string s = a[i].substr(j, l-j) + a[i].substr(0, j);
+
+                if (s1 == s) {
+                    //cout << s1 << ' ' << s << endl;
+                    //cout << i << ' ' << k << ' ' << dp[i-1][k] << ' ' << j << '\n';
+                    dp[i][k] = min(dp[i-1][k] + j, dp[i][k]);
                     break;
                 }
-                rs += lr;
             }
         }
-        res = min(res, rs);
     }
 
-    if (!fnd) {
-        cout << res << '\n';
-    } else {
-        cout << "-1\n";
+    int res = 1e9;
+
+    for (int i = 0; i<l; ++i) {
+        res = min(res, dp[n-1][i]);
     }
-    
-   
+
+    if (res == 1e9) {
+        cout << "-1\n";
+    } else {
+        cout << res << '\n';
+    }
+
     return 0;
 }
