b018be7a51620016af0c6b074aea83816b3022af
alexfinger21 <61606770+alexfinger21@users.noreply.github.com>
2025-02-17 01:42:08 -0500

    Update median-of-two-sorted-arrays.js

--- median-of-two-sorted-arrays.js
+++ median-of-two-sorted-arrays.js
@@ -3,71 +3,23 @@
  * @param {number[]} nums2
  * @return {number}
  */
-
-function binSearch(goal, arr) {
-    let l = 0, r = arr.length - 1
-
-    while (l < r) {
-        const mid = (r + l) >> 1
-
-        if (arr[mid] > goal) {
-            r = mid-1
-        } else if (mid < r && arr[mid+1] <= goal) {
-            l = mid + 1
-        } else {
-            return r
-        }
-    }
-
-    return r
-}
-
 var findMedianSortedArrays = function(nums1, nums2) {
-    const big = nums1.length > nums2.length ? nums1 : nums2 
-    const small = nums1.length > nums2.length ? nums2 : nums1
+    let i1 = 0, i2 = 0;
+    const len = (nums1.length + nums2.length)
 
-    const med = (big.length + small.length) >> 1 + 1
+    let prev = 0
+    let cur = 0
 
-    let l = 0, r = big.length - 1
-
-    while (l < r) {
-        const mid = (l + r) >> 1
-
-        const idx = binSearch(big[mid], small)
-        console.log(mid, idx)
-        if (mid + idx + 2 > med) {
-            r = mid
-        } else if (mid + idx + 2 < med) {
-            l = mid+1
+    for (let i = 0; i<=len >> 1; ++i) {
+        prev = cur
+        if ((i1 < nums1.length) && (i2 == nums2.length || nums1[i1] < nums2[i2])) {
+            cur = nums1[i1]
+            ++i1
         } else {
-            if ((big.length + small.length) % 2) {
-                return big[mid]
-            }
-
-            return big[mid] + Math.max((idx >= 0 ? small[idx] : big[mid-1]) + (mid > 0 ? big[mid-1] : small[idx]))
+            cur = nums2[i2]
+            ++i2
         }
     }
 
-    l = 0, r = small.length - 1
-
-    while (l < r) {
-        const mid = (l + r) >> 1
-
-        const idx = binSearch(small[mid], big)
-        console.log(mid, idx)
-        if (mid + idx + 2 > med) {
-            r = mid
-        } else if (mid + idx + 2 < med) {
-            l = mid+1
-        } else {
-            if ((big.length + small.length) % 2) {
-                return small[mid]
-            }
-
-            return small[mid] + Math.max((idx >= 0 ? big[idx] : small[mid-1]) + (mid > 0 ? small[mid-1] : big[idx]))
-        }
-    }
-    return 0
-}
-
-console.log("RES", findMedianSortedArrays([1, 3], [2]))
+    return len % 2 ? cur : ((cur + prev)/2)
+};
