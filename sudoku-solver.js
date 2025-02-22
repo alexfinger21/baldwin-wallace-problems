52fc5583a1248bd06a1e54a2299634da68d29a4e
alexfinger21 <61606770+alexfinger21@users.noreply.github.com>
2025-02-22 01:28:46 -0500

    Update sudoku-solver.js

--- sudoku-solver.js
+++ sudoku-solver.js
@@ -9,24 +9,17 @@ for (let i = 0; i<9; ++i) {
     nums.push(String(i+1))
 }
 
-var solveSudoku = function(board) {
-    let poss = new Array(9)
-    for (let i = 0; i<9; ++i) {
-        poss[i] = new Array(9)
-        for (let j = 0; j<9; ++j) {
-            poss[i][j] = new Set()
+var solveSudoku = function(bb) {
+    
+    const upd = function(board) {
+        const squares = new Array(9)
+        for (let i = 0; i<9; ++i) {
+            squares[i] = new Array(9)
+            for (let j = 0; j<9; ++j) {
+                squares[i][j] = board[Math.floor(i/3)*3+Math.floor(j/3)][(i%3)*3 + j%3]
+            }
         }
-    }
-
-    let squares = new Array(9)
-    for (let i = 0; i<9; ++i) {
-        squares[i] = new Array(9)
-        for (let j = 0; j<9; ++j) {
-            squares[i][j] = board[Math.floor(i/3)*3+Math.floor(j/3)][(i%3)*3 + j%3]
-        }
-    }
-    const upd = function() {
-        poss = new Array(9)
+        const poss = new Array(9)
         for (let i = 0; i<9; ++i) {
             poss[i] = new Array(9)
             for (let j = 0; j<9; ++j) {
@@ -59,90 +52,59 @@ var solveSudoku = function(board) {
                 }
             }
         }
+        return poss
     }
 
-    const solv = function() {
-        upd()
-        let prevUnsolved = 0
-        let unsolvedCount = 1
-        while (unsolvedCount && !(prevUnsolved == unsolvedCount)) {
-            prevUnsolved = unsolvedCount
-            unsolvedCount = 0;
-            for (let i = 0; i<9; ++i) {
-                for (let j = 0; j<9; ++j) {
-                    if (board[i][j] == '.') {
-                        if (poss[i][j].size >= 8) {
-                            const missing = nums.find(n => !poss[i][j].has(n))  
-                            if (!missing) {
-                                if (i == 6 && j == 6) {
-                                    //console.log("====", board, "====")
-                                }
-                                return 0
-                            }
-                            board[i][j] = missing
-                            poss[i][j].add(missing)
+    const solv = function(board) {
+        
+        const poss = upd(board)
 
-                            const sqr = Math.floor(i/3) * 3 + Math.floor(j/3)
-                            //console.log(missing, squares[sqr], poss[i][j], i, j)
-                            const idx = (i - Math.floor(i/3) * 3) * 3 + j - Math.floor(j/3)*3
-                            squares[sqr][idx] = missing 
-                            upd()
-                        } else {
-                            ++unsolvedCount
-                        }
-                    }
+        let dots = 0
+        let mx = 0, mxi = []
+        for (let i = 0; i<9; ++i) {
+            for (let j = 0; j<9; ++j) {
+                if (board[i][j] == '.') {
+                    ++dots
+                }
+                if (board[i][j] == '.' && poss[i][j].size > mx) {
+                    mx = poss[i][j].size
+                    mxi = [i, j]
                 }
             }
-            
         }
-        if (unsolvedCount) {
-            return 1
-        }
-        return 2
-    }
-    let step = 0
-    const superSolve = function() {
-        const stepp = step
-        ++step
-        const sRes = solv()
-        //if (board[0][6] == "6") {
-            //console.log("SRES", step, sRes)
-        //}
-        if (sRes == 0) {
-            return -1
-        } else if (sRes == 1) {
-            let mx = 0, mxi = []
-            for (let i = 0; i<9; ++i) {
-                for (let j = 0; j<9; ++j) {
-                    if (poss[i][j].size < 8 && poss[i][j].size > mx) {
-                        mx = poss[i][j].size
-                        mxi = [i, j]
-                    }
-                }
+
+        const nms = nums.filter(n => !poss[mxi[0]][mxi[1]].has(n))
+        
+        if (nms.length) {
+            //console.log(dots, mxi, nms)
+            if (dots == 1) {
+                const board_cp = JSON.parse(JSON.stringify(board))
+                board_cp[mxi[0]][mxi[1]] = nms[0]
+                //console.log(board_cp)
+                return board_cp
             }
-
-            const nms = nums.filter(n => !poss[mxi[0]][mxi[1]].has(n))
-
-            const boardd = JSON.parse(JSON.stringify(board))
-            const squaress = JSON.parse(JSON.stringify(squares))
-
-
-            for (const n of nms) {
-                board = boardd
-                squares = squaress
-                //console.log(n, mxi, "====", board[mxi[0]][mxi[1]], poss[mxi[0]][mxi[1]])
-                board[mxi[0]][mxi[1]] = n
-                const r = superSolve()
-                if (board[0][7] == "5") {
-                    console.log(mxi, n, r, stepp, "og", boardd, "new", board)
+            for (let k = 0; k<nms.length; ++k) {
+                if (nms.length > 1) {
+                    //console.log(nms, nms[k])
                 }
+                const board_cp = JSON.parse(JSON.stringify(board))
+                board_cp[mxi[0]][mxi[1]] = nms[k]
+                const r = solv(board_cp)
                 if (r != -1) {
-                    return 1
+                    return r
                 }
             }
-            return -1
-        }
-        return 1
+        } 
+
+        return -1
     }
-    superSolve()
+
+    const bb_c = solv(bb)
+    //console.log("BIBIBABA",bb_c)
+
+    for (let i = 0; i<9; ++i) {
+        for (let j = 0; j<9; ++j) {
+            bb[i][j] = bb_c[i][j]
+        }
+    }
 };
