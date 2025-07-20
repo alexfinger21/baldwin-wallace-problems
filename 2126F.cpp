152da30245e67e97988e69f1e9fde894e7b37dde
alexfinger21 <cdx9933@gmail.com>
2025-07-20 18:19:46 -0400

    code

--- 2126F.cpp
+++ 2126F.cpp
@@ -1,79 +1,77 @@
 #include <bits/stdc++.h>
+
+using ll=long long;
 using namespace std;
-using ll = long long;
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t;
     cin >> t;
-    const int B = 450;  
 
-    while(t--){
-
+    while (t--) {
         int n, q;
         cin >> n >> q;
-        vector<int> col(n);
-        for(int i = 0; i < n; ++i) cin >> col[i];
-        vector<vector<pair<int,int>>> adj(n);
-        vector<tuple<int,int,int>> E;
-        for(int i = 0, u, v, w; i < n-1; ++i){
-            cin >> u >> v >> w; --u; --v;
-            adj[u].emplace_back(v,w);
-            adj[v].emplace_back(u,w);
-            E.emplace_back(u,v,w);
+
+        vector<int> a(n);
+
+        for (int i = 0; i<n; ++i) {
+            cin >> a[i];
         }
-        vector<bool> is_h(n);
-        vector<int> hid(n, -1), hv;
-        for(int i = 0; i < n; ++i) if(adj[i].size() >= B){
-            hid[i] = hv.size();
-            hv.push_back(i);
-            is_h[i] = 1;
-        }
-        int H = hv.size();
-        vector<unordered_map<int,ll>> hm(H);
-        ll ans = 0;
-        for(auto &[u,v,w]: E){
-            if(col[u] != col[v]) ans += w;
-            if(is_h[u]) hm[hid[u]][col[v]] += w;
-            if(is_h[v]) hm[hid[v]][col[u]] += w;
-        }
-        vector<vector<pair<int,int>>> hh(H);
-        for(int i = 0; i < H; ++i){
-            int u = hv[i];
-            for(auto &p: adj[u]) if(is_h[p.first])
-                hh[i].push_back(p);
-        }
-        while(q--){
-            int v, x; cin >> v >> x; --v;
-            int old = col[v];
-            if(old == x){
-                cout << ans << '\n'; continue; 
+
+        vector<unordered_map<ll, ll>> verts(n);
+        vector<ll> sums(n);
+        vector<vector<pair<ll, ll>>> adj(n);
+
+        ll res = 0;
+        for (int i = 0; i<n-1; ++i) {
+            int u, v, c;
+
+            cin >> u >> v >> c;
+            --u; --v;
+
+            verts[u][a[v]] += c;
+            verts[v][a[u]] += c;
+            adj[v].emplace_back(u, c);
+            adj[u].emplace_back(v, c);
+
+            if (a[u] != a[v]) {
+                sums[a[v]] = c;
+                sums[a[u]] = c;
+                res += c;
             }
-            if(is_h[v]){
-                int id = hid[v];
-                ans += hm[id][old] - hm[id][x];
-                for(auto &p: hh[id]){
-                    int u = p.first, w = p.second;
-                    hm[hid[u]][old] -= w;
-                    hm[hid[u]][x] += w;
-                }
-            } else {
-                for(auto &p: adj[v]){
-                    int u = p.first, w = p.second;
-                    if(col[u] == old) ans += w;
-                    if(col[u] == x)   ans -= w;
-                    if(is_h[u]){
-                        hm[hid[u]][old] -= w;
-                        hm[hid[u]][x] += w;
-                    }
+        }
+
+
+        for (int i = 0; i<q; ++i) {
+            int v, x;
+            cin >> v >> x;
+            --v;
+
+            if (a[v] == x) {
+                cout << res << '\n';
+                continue;
+            }
+
+            ll old_s = sums[v];
+            sums[v] -= verts[v][x];
+            sums[v] += verts[v][a[v]];
+            res += sums[v] - old_s;
+
+            for (auto z : adj[v]) {
+                verts[z.first][a[v]] -= z.second;
+                verts[z.first][x] += z.second;
+
+                if (x != a[z.first]) {
+                    sums[z.first] += z.second;
                 }
             }
-            col[v] = x;
-            cout << ans << '\n';
+
+            a[v] = x;
+
+            cout << res << '\n';
         }
     }
+
     return 0;
 }
-
