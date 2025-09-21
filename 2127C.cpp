f1b379271468ff7f533bfc7848f47d29f248a620
alexfinger21 <cdx9933@gmail.com>
2025-09-20 23:53:44 -0400

    ok

---
+++ 2127C.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using ll=long long;
+using namespace std;
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> a(n), b(n);
+        for (int i = 0; i<n; ++i) {
+            cin >> a[i];
+        }
+
+        for (int i = 0; i<n; ++i) {
+            cin >> b[i];
+        }
+
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+    }
+
+    return 0;
+}
