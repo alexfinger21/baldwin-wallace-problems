01c8075963e9a3bf87ffab9459f4fd8645681765
alexfinger21 <cdx9933@gmail.com>
2026-02-14 17:42:11 -0500

    if u go in reverse order the values don't double count

---
+++ binomial-coefficient.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using ll=long long;
+using namespace std;
+
+int32_t main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n, k;
+
+    cin >> n >> k;
+
+    if (k > n) {
+        cout << "come on man\n";
+    }
+
+    vector<ll> dp(k+1);
+
+    for (ll i = 0; i<=n; ++i) {
+        for (ll j = min(i, k); j>=0; --j) {
+            if (j == 0 || j == i) {
+                dp[j] = 1;
+            } else {
+                dp[j] += dp[j-1]; 
+            }
+        }
+    }
+
+    cout << dp[k] << '\n';
+
+    return 0;
+}
