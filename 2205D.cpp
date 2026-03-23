4eeffbe6c59dfad06f3086891c6905d12079cb06
alexfinger21 <cdx9933@gmail.com>
2026-03-22 21:54:43 -0400

    finally solved boom

--- 2205D.cpp
+++ 2205D.cpp
@@ -19,105 +19,44 @@ int32_t main() {
 
         for (int i = 0; i<n; ++i) {
             cin >> a[i];
-
-            indices[a[i]] = i;
         }
 
-        vector<int> a_sorted(a);
-        sort(a_sorted.begin(), a_sorted.end());
+        vector<int> dp(n);
+        vector<int> dp1(n);
+        dp[0] = 1;
+        vector<int> prev;
+        prev.push_back(a[0]);
 
-        vector<int> peaks;
-        map<int, int> peak_idx;
-        map<int, int> sorted_peak_idx;
-        vector<int> smaller_left(n);
-        vector<int> smaller_right(n);
-
-        for (int i = 0; i<n; ++i) {
-            if (i > 0 && i < n-1 && a[i-1] < a[i] && a[i+1] < a[i]) {
-                peaks.push_back(a[i]);
-                peak_idx[a[i]] = i;
+        for (int i = 1; i<n; ++i) {
+            while (!prev.empty() && prev.back() <= a[i]) {
+                prev.pop_back();
             }
-        }
 
+            prev.push_back(a[i]);
+            dp[i] = prev.size();
+        } 
 
-        sort(peaks.begin(), peaks.end());
+        dp1[n-1] = 1;
+        prev = {a[n-1]};
 
-        for (int i = 0; i<peaks.size(); ++i) {
-            sorted_peak_idx[peaks[i]] = i;
-        }
-
-
-        vector<int> min_peak(peaks.size());
-
-        for (int i = 0; i<peaks.size(); ++i) {
-            int t = peaks[i];
-            int l_count = 0;
-            int r_count = 0;
-            int l = 0;
-            int r = 0;
-
-            if (indices[t] != -1) {
-                // // cout << "peak: " << t << endl;
-                int j = indices[t] - 1;
-                int prev_peak = -1;
-
-                while (j >= 0) {
-                    if (a[j] > t) {
-                        break;
-                    }
-
-                    if (peak_idx.count(a[j]) && sorted_peak_idx[a[j]] >= prev_peak) {
-                        if (prev_peak != -1) {
-                            r -= min_peak[sorted_peak_idx[prev_peak]];
-                        }
-                        prev_peak = sorted_peak_idx[a[j]];
-                        r += min_peak[sorted_peak_idx[a[j]]];
-                        // cout << "MIN PEAK L: " <<min_peak[sorted_peak_idx[a[j]]] << ' ' << a[j] << endl; 
-                    } 
-
-
-                    ++l_count;
-                    ++l;
-
-                    --j;
-                }
-
-
-                j = indices[t] + 1;
-                prev_peak = -1;
-
-                while (j < n) {
-                    if (a[j] > t) {
-                        break;
-                    }
-
-                    if (peak_idx.count(a[j]) && sorted_peak_idx[a[j]] >= prev_peak) {
-                        if (prev_peak != -1) {
-                            r -= min_peak[sorted_peak_idx[prev_peak]];
-                        }
-                        prev_peak = sorted_peak_idx[a[j]];
-                        l += min_peak[sorted_peak_idx[a[j]]];
-                        // cout << "MIN PEAK R: " <<min_peak[sorted_peak_idx[a[j]]] << endl; 
-                    } 
-
-                    ++r;
-                    ++r_count;
-
-                    ++j;
-                }
-
-                // // cout << l_count << ' ' << r_count << endl;
-
-                for (int j = peak_idx[peaks[i]] - l_count; j<=peak_idx[peaks[i]] + r_count; ++j) {
-                    indices[a[j]] = -1;
-                }
-
-                min_peak[i] = min(l, r);
-                // cout << t << ' ' << min_peak[i] << endl;
+        for (int i = n-2; i>=0; --i) {
+            while (!prev.empty() && prev.back() <= a[i]) {
+                prev.pop_back();
             }
+
+            prev.push_back(a[i]);
+            dp1[i] = prev.size();
+        } 
+
+        int mx = 0;
+
+        for (int i = 0; i<n-1; ++i) {
+            // cout << dp[i] << ' ' << dp1[i+1] << endl;
+            mx = max(mx, dp[i] + dp1[i+1]);
         }
 
-        cout << (peaks.size() ? (min_peak[peaks.size()-1]) : 0) << '\n';
+        cout << n - mx << '\n';
+
     }
 
     return 0;
