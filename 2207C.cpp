7d138b36e0a6632536939ec05b375d51a98fe0d4
alexfinger21 <cdx9933@gmail.com>
2026-05-03 19:07:02 -0400

    better solution

--- 2207C.cpp
+++ 2207C.cpp
@@ -17,67 +17,37 @@ int32_t main() {
         vector<ll> dp_l(n);
         vector<ll> dp_r(n);
         
-        vector<pair<ll, ll>> rng;
-        ll sm = 0;
+        vector<ll> rng;
 
         vector<ll> heights(n);
 
         for (int i = 0; i<n; ++i) {
             cin >> heights[i];
 
-            int new_i = i;
-
-            while (!rng.empty() && rng.back().first < heights[i]) {
-                pair<ll, ll> last_h = rng.back();
+            while (!rng.empty() && heights[rng.back()] < heights[i]) {
                 rng.pop_back();
-
-                if (!rng.empty()) {
-                    sm -= (h - last_h.first) * (new_i - rng.back().second - 1);
-                    sm += (h - heights[i]) * (new_i - rng.back().second - 1);
-
-                    new_i = last_h.second;
-                } else {
-                    sm -= (h - last_h.first) * (new_i);
-                    sm += (h - heights[i]) * (new_i);
-                }
             }
 
-            sm += h - heights[i];
-            dp_l[i] = sm;
+            dp_l[i] = (i - (!rng.empty() ? rng.back() : -1)) * (h - heights[i]) 
+                + (!rng.empty() ? dp_l[rng.back()] : 0);
 
-            rng.emplace_back(heights[i], i);
+            rng.push_back(i);
 
             cout << dp_l[i] << endl;
         }
 
         rng.clear();
-        sm = 0;
 
         for (int i = n-1; i>=0; --i) {
-            int new_i = i;
-
-            while (!rng.empty() && rng.back().first < heights[i]) {
-                pair<ll, ll> last_h = rng.back();
+            while (!rng.empty() && heights[rng.back()] < heights[i]) {
                 rng.pop_back();
-
-                if (!rng.empty()) {
-                    sm -= (h - last_h.first) * (rng.back().second - new_i - 1);
-                    sm += (h - heights[i]) * (rng.back().second - new_i - 1);
-
-                    new_i = last_h.second;
-                } else {
-                    cout << "sum: " << sm << endl;
-                    sm -= (h - last_h.first) * (n-new_i-1);
-                    sm += (h - heights[i]) * (n-new_i-1);
-
-                    cout << "add: " << (h - heights[i]) * (n-new_i-1) << endl;
-                }
             }
 
-            sm += h - heights[i];
-            dp_r[i] = sm;
 
-            rng.emplace_back(heights[i], i);
+            dp_r[i] = ((!rng.empty() ? rng.back() : n) - i) * (h - heights[i])
+                + (!rng.empty() ? dp_r[rng.back()] : 0);
+
+            rng.emplace_back(i);
 
             cout << dp_r[i] << endl;
         }
