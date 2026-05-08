6960bc8c7340169628ee2cfbe2ad73e70212956c
alexfinger21 <cdx9933@gmail.com>
2026-05-08 10:12:35 -0400

    codeforces

--- 2227D.cpp
+++ 2227D.cpp
@@ -14,10 +14,13 @@ int32_t main() {
         cin >> n;
 
         vector<pair<int, int>> ind(n, {-1, 0});
+        vector<int> a(2*n);
 
         for (int i = 0; i<2*n; ++i) {
             int p;
             cin >> p;
+
+            a[i] = p;
 
             if (ind[p].first == -1) {
                 ind[p].first = i;
@@ -25,7 +28,65 @@ int32_t main() {
                 ind[p].second = i;
             }
         } 
-    }
+
+        int ctr = (ind[0].first + ind[0].second) / 2;
+        set<int> nums;
+
+        if (n > 1) {
+            int c = (ind[1].first + ind[1].second) / 2;                
+
+            if (c != ctr) {
+                if (ind[0].first == c || ind[0].second == c) {
+                    ctr = c;
+                } 
+            }
+        }
+
+
+        for (int i = ctr+1; i<2*n; ++i) {
+            if (((ind[a[i]].first) + (ind[a[i]].second)) / 2 == ctr) {
+                nums.insert(a[i]);
+            } else {
+                break;
+            }
+        }
+
+        int res = 0;
+
+        for (auto x : nums) {
+            if (x - res <= 1) {
+                res = x;
+            } else {
+                break;
+            }
+        }
+        
+        ++res;
+
+        nums.clear();
+
+        for (int i = ctr-1; i<2*n; ++i) {
+            if (((ind[a[i]].first) + (ind[a[i]].second)) / 2 == ctr) {
+                nums.insert(a[i]);
+            } else {
+                break;
+            }
+        }
+
+        int res2 = 0;
+
+        for (auto x : nums) {
+            if (x - res2 <= 1) {
+                res2 = x;
+            } else {
+                break;
+            }
+        }
+
+        ++res2;
+
+        cout << (min(res, res2) + ((ind[0].second - ind[0].first) % 2 ? 0 : 1)) << '\n';
+    } 
 
     return 0;
 }
