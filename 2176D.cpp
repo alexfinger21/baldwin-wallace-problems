7cb36ec21b80c3dd0945d4983f0d5cdc1c9d09a1
alexfinger21 <cdx9933@gmail.com>
2026-05-25 15:22:20 -0400

    solved

--- 2176D.cpp
+++ 2176D.cpp
@@ -3,27 +3,8 @@
 using ll=long long;
 using namespace std;
 
-ll MOD = 998244353;
-ll res = 0;
+const ll MOD = 998244353;
 
-ll solve(int idx, vector<ll>& dp, vector<bool>& vis, vector<pair<int, int>>& edges, vector<ll>& vals, vector<vector<int>>& vert) {
-    if (!vis[idx]) {
-        dp[idx] += 1;
-        vis[idx] = true;
-    } else {
-        return dp[idx];
-    }
-
-    for (int e : vert[edges[idx].second]) {
-        if (vals[edges[e].second] == (vals[edges[idx].first] + vals[edges[idx].second])) {
-            dp[idx] = (dp[idx] + solve(e, dp, vis, edges, vals, vert)) % MOD;
-        }
-
-    }
-
-    res = (res + dp[idx]) % MOD;
-    return dp[idx];
-}
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
@@ -32,49 +13,51 @@ int32_t main() {
     cin >> t;
 
     while (t--) {
-        res = 0;
-
         int n, m;
+
         cin >> n >> m;
 
-        vector<vector<int>> vert(n);
-        vector<pair<int, int>> edges(m);
-        vector<ll> vals(n);
-        vector<bool> visited(m);
-        vector<ll> dp(m);
-
+        vector<ll> a(n);
+        
         for (int i = 0; i<n; ++i) {
-            cin >> vals[i];
+            cin >> a[i];
         }
 
-        for (int i = 0; i<m; ++i) {
-            int s, e;
-            cin >> s >> e;
-
-            --s;
-            --e;
-
-            edges[i].first = s;
-            edges[i].second = e;
-
-            vert[s].push_back(i);
-        }        
-
+        vector<vector<int>> g(n);
+        vector<pair<int, int>> edges(m);
 
         for (int i = 0; i<m; ++i) {
-            if (!visited[i]) {
-                solve(i, dp, visited, edges, vals, vert);
-            }
+            int u, v;
+
+            cin >> u >> v;
+            --u;
+            --v;
+
+            g[u].push_back(v);
+
+            edges[i].first = u;
+            edges[i].second = v;
         }
 
-        /*
-        for (int i = 0; i<m; ++i) {
-            // res += dp[i];
-            cout << dp[i] << endl;
-        }
-        */
+        sort(edges.begin(), edges.end(), [&a](pair<int, int> p1, pair<int, int> p2) {
+            return (a[p1.first] + a[p1.second]) > (a[p2.first] + a[p2.second]);
+        });
 
-        cout << (res % MOD) << '\n';
+        vector<map<ll, int>> dp(n);
+
+        ll res = 0;
+
+        for (auto e : edges) {
+            ll s = a[e.first] + a[e.second];
+            ll cdp = dp[e.second][s];
+
+            cdp = (cdp + 1) % MOD;
+            dp[e.first][a[e.second]] = (cdp + dp[e.first][a[e.second]]) % MOD;
+
+            res = (res + cdp) % MOD;
+        }
+
+        cout << res << '\n';
     }
 
     return 0;
