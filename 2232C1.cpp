119334dda6871a1a228dd085834fb79260b3e008
alexfinger21 <cdx9933@gmail.com>
2026-06-12 20:33:39 -0400

    solved, awesome work team

--- 2232C1.cpp
+++ 2232C1.cpp
@@ -16,61 +16,110 @@ int32_t main() {
         cin >> n >> x >> s;
         cin >> mem;
 
-        pair<ll, ll> dp_x{0, x};
-        pair<ll, ll> dp_y{0, x};
+        ll space = 0;
+        ll intro = x;
+
+        vector<ll> missing(n);
+
+        bool found_a = false;
+        vector<ll> ambi_miss;
+        vector<int> indx;
+        map<int, int> min_cnt;
 
         for (int i = 0; i<n; ++i) {
-            if (mem[i] == 'I') {
-                if (dp_x.second > 0) {
-                    --dp_x.second;
-                    dp_x.first += s - 1;
-                }
-
-                if (dp_y.second > 0) {
-                    --dp_y.second;
-                    dp_y.first += s - 1;
-                }
-            }
-
-            if (mem[i] == 'E') {
-                if (dp_x.first > 0) {
-                    --dp_x.first;
-                }
-
-                if (dp_y.first > 0) {
-                    --dp_y.first;
-                }
-            }
-
             if (mem[i] == 'A') {
-                if (((x - dp_x.second) * s - dp_x.first) > ((x - dp_y.second) * s - dp_y.first)) {
-                    if (dp_x.second > 0) {
-                        dp_y = dp_x;
-                    }
-                } else if (((x - dp_x.second) * s - dp_x.first) < ((x - dp_y.second) * s - dp_y.first)) {
-                    if (dp_y.first > 0) {
-                        dp_x = dp_y;
-                    }
+                found_a = true;
+                if (ambi_miss.size()) {
+                    ++min_cnt[ambi_miss.back()];
                 }
 
-                if (dp_x.first > 0) {
-                    --dp_x.first;
+                ambi_miss.push_back(space);
+                indx.push_back(i);
+            } else if (mem[i] == 'I') {
+                if (intro > 0) {
+                    space += s - 1;
+                    --intro;
+                    missing[i] = space;
+                    if (found_a) {
+                        ambi_miss[ambi_miss.size() - 1] = min(ambi_miss[ambi_miss.size() - 1], space);
+                    }
                 }
-
-                if (dp_y.second > 0) {
-                    --dp_y.second;
-                    dp_y.first += s - 1;
+            } else {
+                if (found_a) {
+                    --space;
+                    ambi_miss[ambi_miss.size() - 1] = min(ambi_miss[ambi_miss.size() - 1], space);
+                    missing[i] = space;
+                } else {
+                    space = max(space - 1, 0LL);
                 }
             }
-
-            /*
-            cout << dp_y.first << ' ' << dp_y.second << endl;
-            cout << dp_x.first << ' ' << dp_x.second << endl;
-            cout << "---\n";
-            */
+            // cout << missing[i] << endl;
         }
 
-        cout << max(x * s - (dp_x.first + dp_x.second * s), x * s - (dp_y.first + dp_y.second * s)) << '\n';
+        if (ambi_miss.size()) {
+            ++min_cnt[ambi_miss.back()];
+        }
+
+        /*
+        cout << "---" << endl;
+
+        for (int i = 0; i<ambi_miss.size(); ++i) {
+            cout << ambi_miss[i] << endl;
+        }
+        */
+
+        vector<int> pref_I(n, 0);
+        int current_I_count = 0;
+        for(int i = 0; i < n; ++i){
+            if(mem[i] == 'I') current_I_count++;
+            pref_I[i] = current_I_count;
+        }
+
+        ll added = 0;
+        int converted_a = 0;  
+
+        for (int i = 0; i < ambi_miss.size(); ++i) {
+            
+            int I_before = (indx[i] == 0 ? 0 : pref_I[indx[i] - 1]) + converted_a;
+
+            if (min_cnt.begin()->first + added <= 0 && I_before < x) {
+                added += s - 1;
+                mem[indx[i]] = 'I';
+                converted_a++;
+            } else {
+                --added;
+                mem[indx[i]] = 'E';
+            }
+
+            --min_cnt[ambi_miss[i]];
+
+            if (!min_cnt[ambi_miss[i]]) {
+                min_cnt.erase(ambi_miss[i]);
+            }
+        }
+
+        //cout << mem << endl;
+
+        added = 0;
+        space = 0;
+        intro = x;
+
+        for (int i = 0; i<n; ++i) {
+            if (mem[i] == 'E') {
+                if (space) {
+                    ++added;
+                    --space;
+                }
+            } else {
+                if (intro) {
+                    ++added;
+                    --intro;
+                    space += s - 1;
+                }
+            }
+        }
+
+        cout << added << '\n';
     }
 
     return 0;
