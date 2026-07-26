70036d23667aeb2b625ecac93b355d5cce18f3cc
alexfinger21 <cdx9933@gmail.com>
2026-07-26 14:13:09 -0400

    seg tree i guess

--- 356A.cpp
+++ 356A.cpp
@@ -1,8 +1,39 @@
-#include <algorithm>
 #include <bits/stdc++.h>
 
 using ll=long long;
 using namespace std;
+
+vector<int> tree;
+vector<int> winners;
+
+void update(int l, int h, int w, int idx, int lw, int up) {
+    if (lw == l && up == h) {
+        if (!tree[idx]) {
+            for (int i = l; i<=h; ++i) {
+                if (i != (w-1) && !winners[i]) {
+                    winners[i] = w;
+                }
+            }
+
+            tree[idx] = w;
+        } else {
+            if (!winners[tree[idx] - 1] && tree[idx] != w) {
+                winners[tree[idx] - 1] = w; 
+            }
+        }
+
+        return;
+    }
+
+    if (l <= (lw + up) / 2) {
+        update(l, min(h, (lw+up) / 2), w, idx*2, lw, (lw+up) / 2);
+    }
+
+    if (h > (lw + up) / 2) {
+        update(max((lw + up)/2 + 1, l), h, w, idx*2 + 1, (lw+up) / 2 + 1, up);
+    }
+
+}
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
@@ -12,34 +43,19 @@ int32_t main() {
 
     cin >> n >> m;
 
-    vector<int> winners(n + 1);
-
-    vector<int> rem(n + 1);
-
-    for (int i = 1; i<=n; ++i) {
-        rem[i] = i; 
-    }
+    tree = vector<int>(4*n);
+    winners = vector<int>(n);
 
     for (int i = 0; i<m; ++i) {
-        int l, h, w;
-        cin >> l >> h >> w;
+        int l, r, w;
+        cin >> l >> r >> w;
 
-        int idx = lower_bound(rem.begin(), rem.end(), l) - rem.begin();
+        --l; --r;
 
-        for (int j = idx; j<rem.size(); ++j) {
-            if (rem[j] > h) {
-                break;
-            }
-
-            if (rem[j] != w) {
-                winners[rem[j]] = w;
-                rem.erase(rem.begin() + j);
-                --j;
-            }
-        }
+        update(l, r, w, 1, 0, n-1);
     }
 
-    for (int i = 1; i<=n; ++i) {
+    for (int i = 0; i<n; ++i) {
         cout << winners[i] << ' ';
     }
 
